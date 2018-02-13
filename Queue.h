4329ec8ea39a05563f2e8aa41617217afed9e6fa
Uxeron <vgabrielius123@gmail.com>
2018-02-13 11:04:08 +0200

    Added comments

--- Queue.h
+++ Queue.h
@@ -1,3 +1,5 @@
+/* 4 gr. 2 pogr. 226 */
+
 #ifndef QUEUE_H_
     #define QUEUE_H_
 
@@ -12,12 +14,12 @@
         int size;
     } queue;
 
-    queue* newQueue();
-    int enqueue(queue* que, int val);
-    int dequeue(queue* que);
-    int full(queue* que);
-    int empty(queue* que);
-    int first(queue* que);
-    int count(queue* que);
-    void erase(queue** que);
+    queue* newQueue();					/* Create a new queue */
+    int enqueue(queue* que, int val);	/* Add an element to the queue. return codes: 0 - command successful, 1 - unable to allocate memory */
+    int dequeue(queue* que);			/* Remove the first element in the queue. return codes: 0 - command successful, 1 - queue is empty */
+    int full(queue* que);				/* Check if queue is full. return codes: 0 - not full, 1 - full */
+    int empty(queue* que);				/* Check if queue is empty. return codes: 0 - not empty, 1 - empty */
+    int first(queue* que);				/* Return first element value */
+    int count(queue* que);				/* Return number of elements in queue*/
+    void erase(queue** que);			/* Erase the entire queue */
 #endif
